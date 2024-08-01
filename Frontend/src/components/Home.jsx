8b1e5b237531d71c29005b18a3149c38184a760d
Nirmal Basnet <157794761+NimsBajey@users.noreply.github.com>
2024-08-01 07:42:36 +0545

    Update Home.jsx

--- Frontend/src/components/Home.jsx
+++ Frontend/src/components/Home.jsx
@@ -1,14 +1,9 @@
-import React, {useState, useContext} from 'react'
+import React from 'react'
 import { useNavigate } from 'react-router-dom'
 import Head from './Head'
-import { NavLink } from 'react-router-dom'
-import Back from '../assets/2.mp4'
-import Baba from '../assets/baba.png'
-import Log from '../assets/1.mp4'
 
-import DiceBox from './DiceBox'
 import Login from './Login'
-import { playerIds } from './function'
+
 
 
 const Home = () => {
